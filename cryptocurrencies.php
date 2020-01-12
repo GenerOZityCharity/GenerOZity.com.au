c112672efd957d049b14f1e65cd1f7a96900a19b
GenerOZity Charity <59790747+GenerOZityCharity@users.noreply.github.com>
2020-01-12 23:15:10 +0800

    First Commit

---
+++ cryptocurrencies.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="//cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.0.0/ekko-lightbox.css" rel="stylesheet">
+<link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta content="https://generozity.com.au/" property="og:url">
+<meta content="website" property="og:type">
+<meta content="GenerOZity" property="og:title">
+<meta content="GenerOZity is a 48 hour pop-up Twitch marathon that brings together content creators from around Australia to raise money for Child's Play." property="og:description">
+<meta content="https://s3-ap-southeast-2.amazonaws.com/generozity/socialmediasquare.png" property="og:image">
+<meta content="summary" name="twitter:card">
+<meta content="@generOZity" name="twitter:site">
+<meta content="GenerOZity" name="twitter:title">
+<meta content="GenerOZity is a 48 hour pop-up Twitch marathon that brings together content creators from around Australia to raise money for Child's Play." name="twitter:description">
+<meta content="https://s3-ap-southeast-2.amazonaws.com/generozity/socialmediasquare.png" name="twitter:image">
+<title>GenerOZity Cryptocurrencies</title>
+</head>
+
+<body>
+
+<div class="py-5 bg-primary">
+	<div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-12">
+          <a href="https://generozity.com.au"><img class="img-fluid d-block mx-auto" src="https://s3-ap-southeast-2.amazonaws.com/generozity/GenerOZity.png" width="72%"></a><br> 
+         </div>
+      </div>
+
+	</div>
+</div>
+
+<div class="bg-faded py-5">
+    <div class="container">
+      <div class="row justify-content-center">
+
+<h1>Bitcoin: 13nnYTvudjaLFNsuGsxCLPPQRma4yt1Sa2</a></h1><br>
+<h1>Litecoin: LhQC2iYAHVLK8H41vLcxgcNqmdSKQMmqNq</h1></a><br>
+<h1>Ethereum: 0x96265404096B8E7233F4e596C6EE170d7C8E32A0</a></h1><br>
+<h1>Zcash: t1dffGTKwc9hj4yc1n96RoGdT1fz42Grbn9</a></h1><br>
+      </div>
+    </div>
+  </div>
+  
+  
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript">
+</script> 
+<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js" type="text/javascript">
+</script> 
+<script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js">
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100353291-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
